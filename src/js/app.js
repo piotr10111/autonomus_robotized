ecd265950074c499e6cb8a0d644b7209ac906a58
Piotr <piotr.michalski2000@gmail.com>
2018-10-12 15:11:41 +0200

    founders; reverse images

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,34 @@
-const plus_btn = document.querySelector('.plus-button');
+(function () {
 
-const show_media = () => {
-    plus_btn.classList.toggle('open');
-    document.querySelector('.social-button').
-    classList.toggle('active');
-}
+    const plus_btn = document.querySelector('.plus-button');
 
-plus_btn.addEventListener('click', show_media);
+    const show_media = () => {
+        plus_btn.classList.toggle('open');
+        document.querySelector('.social-button').
+        classList.toggle('active');
+    }
 
-/*plus_btn.addEventListener('click', () => {
-    this.classList.toggle('open')
-})*/
+    plus_btn.addEventListener('click', show_media);
+
+
+    // Hamburger menu
+    const button = document.querySelector('.header__button');
+    button.addEventListener('click', function (event) {
+        event ? event : event = window.event;
+        event.preventDefault();
+        this.classList.toggle('.header__button--close');
+        console.log('close')
+    })
+
+    if (window.innerWidth <= 1000) {
+        var hide = function hide() {
+            var ul = document.querySelector('.nav__list');
+            ul.classList.toggle('nav__list--block');
+
+            //var button = document.querySelector('.header__button');
+            button.classList.toggle('header__button--close');
+        };
+
+    }
+
+})();
