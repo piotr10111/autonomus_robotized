4b02c439eacf96acf223ff9f889ca8e85fe3e380
Piotr <piotr.michalski2000@gmail.com>
2018-10-13 19:10:57 +0200

    speakers section

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,28 @@
 (function () {
 
-    const plus_btn = document.querySelector('.plus-button');
-
-    const show_media = () => {
-        plus_btn.classList.toggle('open');
-        document.querySelector('.social-button').
-        classList.toggle('active');
-    }
-
-    plus_btn.addEventListener('click', show_media);
-
-
     // Hamburger menu
     const button = document.querySelector('.header__button');
     button.addEventListener('click', function (event) {
         event ? event : event = window.event;
         event.preventDefault();
-        this.classList.toggle('.header__button--close');
+        this.classList.toggle('header__button--close');
         console.log('close')
+        var ul = document.querySelector('.nav__list');
+        ul.classList.toggle('nav__list--block');
     })
 
     if (window.innerWidth <= 1000) {
-        var hide = function hide() {
-            var ul = document.querySelector('.nav__list');
-            ul.classList.toggle('nav__list--block');
+        const hide = function() {
+            
 
             //var button = document.querySelector('.header__button');
             button.classList.toggle('header__button--close');
         };
+
+        const links = Array.from(document.querySelectorAll('.clicked'));
+        links.forEach(function (link) {
+            return link.addEventListener('click', hide);
+        });
 
     }
 
