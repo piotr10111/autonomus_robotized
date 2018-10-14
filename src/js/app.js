168424fcf909e21026448b5692b5238aa6d25801
Piotr <piotr.michalski2000@gmail.com>
2018-10-14 17:12:25 +0200

    origin master

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,18 @@
+
+'use strict';
 (function () {
 
     // Hamburger menu
     const button = document.querySelector('.header__button');
+    const ul = document.querySelector('.nav__list');
     button.addEventListener('click', function (event) {
         event ? event : event = window.event;
         event.preventDefault();
         this.classList.toggle('header__button--close');
         console.log('close')
-        var ul = document.querySelector('.nav__list');
         ul.classList.toggle('nav__list--block');
-    })
+    });
+
 
     if (window.innerWidth <= 1000) {
         const hide = function() {
@@ -19,11 +22,18 @@
             button.classList.toggle('header__button--close');
         };
 
-        const links = Array.from(document.querySelectorAll('.clicked'));
+        const links =[...document.querySelectorAll('.clicked')];
         links.forEach(function (link) {
             return link.addEventListener('click', hide);
         });
 
     }
+
+    // Smooth Scroll
+    new ScrollMenu('.scroll', {
+        duration: 900,
+        //activeOffset: 100,
+        scrollOffset: 0
+    });
 
 })();
